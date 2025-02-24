d63e1f18c8df400cf8a4a8af686d938f9ac48c13
Lystopad0 <fantom.sasha@gmail.com>
2025-02-24 22:19:14 +0200

    update docker files, add AUTHENTICATION, PERMISSION

--- event_management/settings.py
+++ event_management/settings.py
@@ -146,6 +146,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
 }
 
 
